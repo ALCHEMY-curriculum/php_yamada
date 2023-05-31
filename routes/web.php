66368ae35aa635001de7920c509e08381c0ce2ed
asd15-pass <ymdr4210@gmail.com>
2023-05-31 22:34:56 +0900

    2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/php_basic_02', function () {
+    return view('php_basic_02');
+});
