790b9e6839ee915d5ce8fe56970f6ddd27984143
asd15-pass <ymdr4210@gmail.com>
2023-06-28 19:59:57 +0900

    09_制御構文 - 繰り返し(for/while)

--- resources/views/php_basic_09.blade.php
+++ resources/views/php_basic_09.blade.php
@@ -13,9 +13,31 @@
         <!-- Styles -->
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body class="antialiased">
+    <body class="antialiased m-6">
         <div >
-            <h1 >Hello World!!!</h1>
+        <?php
+
+echo '<table>';
+for ($i = 1; $i <= 31; $i++) {
+  if ($i % 7 === 1) {
+    echo '<tr>';
+  }
+  
+  if ($i  === 1 || $i === 8 || $i === 15 || $i === 22 || $i === 29) {
+    echo '<td style="padding: 1rem"class="bg-red-600 border-2">' . $i.'（日）'. '</td>';
+  }elseif($i % 7 === 0){
+    echo '<td style="padding: 1rem"class="bg-blue-600 border-2">' . $i.'（土）' . '</td>';
+    echo '</tr>';
+  }else{
+    echo '<td style="padding: 1rem" class="border-2">' . $i . '</td>';
+  }
+  
+}
+echo '</table>';
+
+
+?>
+        
         </div>
     </body>
 </html>
