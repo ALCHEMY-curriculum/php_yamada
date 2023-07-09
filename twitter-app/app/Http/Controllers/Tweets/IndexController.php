1b28d987c6a1b462df35559ca6069d126e27931d
asd15-pass <ymdr4210@gmail.com>
2023-07-09 15:55:48 +0900

    境構築を回のブランチでやっちゃってたので旦マージしてます

--- twitter-app/app/Http/Controllers/Tweets/IndexController.php
+++ twitter-app/app/Http/Controllers/Tweets/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tweets;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//laravel課題１と２まとめてみてください//
 class IndexController extends Controller {
     public function __invoke(Request $request)
     {
