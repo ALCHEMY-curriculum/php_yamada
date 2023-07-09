39bf4f57b48e064ca038c3271e8460fe8722be51
asd15-pass <ymdr4210@gmail.com>
2023-07-09 15:19:28 +0900

    Twitter風アプリ作成02-HTMLを表示

--- twitter-app/app/Http/Controllers/Tweets/IndexController.php
+++ twitter-app/app/Http/Controllers/Tweets/IndexController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller {
-    public function __invoke(Request $request) {
-        return 'hello single action';
+    public function __invoke(Request $request)
+    {
+        return view('tweets.index', ['name' => 'Alchemy']);
     }
 }
 class ShowTweetController extends Controller {
     public function __invoke($id) {
-        return "hello {$id}";
+        return view('tweets.index', ['name' => 'Alchemy'.$id]);
     }
 }
