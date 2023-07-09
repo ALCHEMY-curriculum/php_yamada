a33fb97a74fb14a09f85ff1868d851c0f9eaf2a0
asd15-pass <ymdr4210@gmail.com>
2023-07-09 15:40:42 +0900

    ブランチ分けるよう

--- twitter-app/resources/views/tweets/show.blade.php
+++ twitter-app/resources/views/tweets/show.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ついったーshow</title>
+    <title>ついったー</title>
   </head>
   <body>
     <h1>show</h1>
